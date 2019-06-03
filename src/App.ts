2c68ce83dc17a6a643e8d9c93aea723cd5763499
Girish <girishkumarv21@gmail.com>
2019-06-03 12:59:35 +0530

    added express and apollo-server-express

--- src/App.ts
+++ src/App.ts
@@ -1,15 +1,45 @@
+import { ApolloServer, gql } from "apollo-server-express";
 import dotenv from "dotenv";
 import express from "express";
+
+import http from "http";
 
 import * as router from "./routes/routes";
 
 dotenv.config();
 
 const app = express();
-
-router.routes(app);
-
-app.listen(8080, () => {
-  // tslint:disable-next-line:no-console
-  console.log("Server Up");
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, PUT, POST, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
 });
+
+const typeDefs = gql`
+  type Query {
+    hello: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello Apollo TS!"
+  }
+};
+
+const apollo = new ApolloServer({
+  resolvers,
+  typeDefs
+});
+
+const server = http.createServer(app);
+
+apollo.applyMiddleware({ app, path: "/" });
+app.listen(8080);
