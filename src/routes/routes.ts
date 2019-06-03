43f72ec70eff64a010c0292938f19ec841add5ac
Girish <girishkumarv21@gmail.com>
2019-06-03 17:17:27 +0530

    removed routes

--- src/routes/routes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-export const routes = (app: express.Application) => {
-  app.get("/", (req, res, next) => {
-    return res.status(200).json({
-      message: "Hello TS!"
-    });
-  });
-};
